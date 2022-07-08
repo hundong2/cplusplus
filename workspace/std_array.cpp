d22c88af487310df4cdec3b4053dd6392e632ea0
donghun lee <ehdgns104@gmail.com>
2022-07-08 06:00:23 +0000

    singleton speed check

--- workspace/std_array.cpp
+++ workspace/std_array.cpp
@@ -1,7 +1,28 @@
+#include <array>
 #include <iostream>
-#include <algorithm>
 
+typedef std::array<int, 4> Myarray;
 int main()
 {
-	return 0;
+    Myarray c0 = { 0, 1, 2, 3 };
+
+    // display contents " 0 1 2 3"
+    for (const auto& it : c0)
+    {
+        std::cout << " " << it;
+    }
+    std::cout << std::endl;
+
+    Myarray c1(c0);
+
+    // display contents " 0 1 2 3"
+    for (const auto& it : c1)
+    {
+        std::cout << " " << it;
+    }
+    std::cout << std::endl;
+	
+	std::for_each(Myarray.begin(), Myarray.end(), [](int x) { std::cout << x <<" "; });
+
+    return (0);
 }
